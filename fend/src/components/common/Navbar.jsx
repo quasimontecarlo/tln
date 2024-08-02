769b3ae9ae3b2cbbb07e031b66bb03553cfd62df
crltt <info@crltt.com>
2024-08-02 18:01:46 +0930

    cleaning up indentation

--- fend/src/components/common/Navbar.jsx
+++ fend/src/components/common/Navbar.jsx
@@ -53,75 +53,75 @@ const Navbar = () => {
 
 	return (
         <div className={classNames("fixed w-full", isMobile && "bottom-0" || "top-0")}>
-		<div className="navbar max-w-[1095px] mx-auto justify-between bg-base-100 z-10">
-            <div className="btn btn-ghost font-m1m_mid font-normal hover:bg-base-100">
-                <Link to={`/`} className="flex text-xl pe-1">
-                    <p className="hidden md:block">the</p>
-                    <p className="hidden md:block text-base-200">lonely</p>
-                    <p className="hidden md:block">network</p>
-                    <p className="md:hidden text-base-200">&gt;</p>
-                    <p className="md:hidden">_</p>
-                </Link>
-            </div>
-            <div role="tablist" className="tabs tabs-bordered">
-                <Link to={write}
-                role="tab"
-                className={classNames(classNames("tab", isMatchingLocation(write) && "tab-active"), !isMobile && "w-28")}
-                >
-                    write
-                </Link>
-                <Link to={home}
-                role="tab"
-                className={classNames(classNames("tab", isMatchingLocation(home) && "tab-active"), !isMobile && "w-28")}
-                >
-                    discovery
-                </Link>
-                <Link to={reading}
-                role="tab"
-                className={classNames(classNames("tab", isMatchingLocation(reading) && "tab-active"), !isMobile && "w-28")}
-                >
-                    reading
-                </Link>
-            </div>
-
-            <div className="">
-                {data && (
-					<Link
-                        to= {pp}
-						className="mt-auto mb-auto flex transition-all duration-100"
-					>
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-square avatar hover:bg-base-100">
-                            <div className="w-8 rounded-md">
-                                <img src={data?.picture || "/avatar-placeholder.png"} />
-                            </div>
-                        </div>
+            <div className="navbar max-w-[1095px] mx-auto justify-between bg-base-100 z-10">
+                <div className="btn btn-ghost font-m1m_mid font-normal hover:bg-base-100">
+                    <Link to={`/`} className="flex text-xl pe-1">
+                        <p className="hidden md:block">the</p>
+                        <p className="hidden md:block text-base-200">lonely</p>
+                        <p className="hidden md:block">network</p>
+                        <p className="md:hidden text-base-200">&gt;</p>
+                        <p className="md:hidden">_</p>
                     </Link>
-                    )}
-                <div className={classNames("dropdown", !isMobile ? "dropdown-end" : "dropdown-top dropdown-left")}>
-                    <div tabIndex={0} className="flex">
-                        <button className="btn btn-ghost hover:bg-base-100 ps-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-                            </svg>
-                      </button>
+                </div>
+                <div role="tablist" className="tabs tabs-bordered">
+                    <Link to={write}
+                    role="tab"
+                    className={classNames(classNames("tab", isMatchingLocation(write) && "tab-active"), !isMobile && "w-28")}
+                    >
+                        write
+                    </Link>
+                    <Link to={home}
+                    role="tab"
+                    className={classNames(classNames("tab", isMatchingLocation(home) && "tab-active"), !isMobile && "w-28")}
+                    >
+                        discovery
+                    </Link>
+                    <Link to={reading}
+                    role="tab"
+                    className={classNames(classNames("tab", isMatchingLocation(reading) && "tab-active"), !isMobile && "w-28")}
+                    >
+                        reading
+                    </Link>
+                </div>
+
+                <div className="">
+                    {data && (
+                        <Link
+                            to= {pp}
+                            className="mt-auto mb-auto flex transition-all duration-100"
+                        >
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-square avatar hover:bg-base-100">
+                                <div className="w-8 rounded-md">
+                                    <img src={data?.picture || "/avatar-placeholder.png"} />
+                                </div>
+                            </div>
+                        </Link>
+                        )}
+                    <div className={classNames("dropdown", !isMobile ? "dropdown-end" : "dropdown-top dropdown-left")}>
+                        <div tabIndex={0} className="flex">
+                            <button className="btn btn-ghost hover:bg-base-100 ps-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                                </svg>
+                        </button>
+                        </div>
+                        <ul tabIndex={0} className={classNames("z-[1] shadow-l bg-base-100 menu items-end menu-sm rounded-md dropdown-content w-auto", isMobile && "translate-x-8")}>
+                            <li><Link
+                                    to= {"/about"}
+                                    className="hover:bg-base-100 focus:bg-base-100">
+                                    credits
+                            </Link></li>
+                            <DeleteUserModal authUser= { data }/>
+                            <li><a
+                            className="hover:bg-base-100 focus:bg-base-100"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                logout();
+                            }}>logout</a></li>
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className={classNames("z-[1] shadow-l bg-base-100 menu items-end menu-sm rounded-md dropdown-content w-auto", isMobile && "translate-x-8")}>
-                        <li><Link
-                                to= {"/about"}
-                                className="hover:bg-base-100 focus:bg-base-100">
-                                credits
-                        </Link></li>
-                        <DeleteUserModal authUser= { data }/>
-                        <li><a
-                        className="hover:bg-base-100 focus:bg-base-100"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            logout();
-                        }}>logout</a></li>
-                    </ul>
                 </div>
             </div>
-        </div>
         </div>
 	);
 };
